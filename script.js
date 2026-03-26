bfa5c3bd20f17db385005e776def6374bb219dd7
wuqianpu-blip <wuqianpu@gmail.com>
2026-03-26 17:37:40 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+const faders = document.querySelectorAll('.fade');
+
+window.addEventListener('scroll', () => {
+  faders.forEach(el => {
+    const top = el.getBoundingClientRect().top;
+    if (top < window.innerHeight - 100) {
+      el.classList.add('show');
+    }
+  });
+});
